09f31098721cd5d02dd871c3632d049a7c27539c
GallupinGallup <gallupingallup@gmail.com>
2017-04-24 13:03:36 -0600

    I created the projects and added the classes.

---
+++ src/Controller/DrawingRunner.java
@@ -0,0 +1,11 @@
+package Controller;
+
+import DrawingController;
+
+public class DrawingRunner {
+	public satatic void main(String [] args)
+	{
+		DrawingController = new DrawingController;
+		controller.start;
+	}
+}
